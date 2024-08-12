aa730f5342bbfaf29ee1dfc72a6b3c9376f673de
Chris Roberts <chrisrobertscollective@gmail.com>
2024-08-12 11:56:25 +0100

    adding toggleTodo fuction

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import  Link  from 'next/link';
+import { redirect } from 'next/navigation';
+import { prisma } from '@/app/db';
 
 async function createTodo(data:FormData) {
   "use server"
-  console.log('hello');
+  const title = data.get("title")?.valueOf();
+  if(typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid title");
+  }
+
+  await prisma.todo.create({data: {title, complete: false}})
+  redirect("/");
 }
 
 export default function Page() {
