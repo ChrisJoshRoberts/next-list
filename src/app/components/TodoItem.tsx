54d2f7f2ccdf4a9b050cb1c373e920fc1d716778
Chris Roberts <chrisrobertscollective@gmail.com>
2024-08-12 12:12:10 +0100

    adding delete button

--- src/app/components/TodoItem.tsx
+++ src/app/components/TodoItem.tsx
@@ -1,11 +1,21 @@
 "use client"
+import { useState } from "react";
 type TodoItemProps = {
   id: string;
   title: string;
   complete: boolean;
   toggleTodo: (id: string, complete: boolean) => void;
+  deleteTodo: (id: string) => void;
 };
-export function TodoItem({id, title, complete, toggleTodo}: TodoItemProps) {
+export function TodoItem({id, title, complete, toggleTodo, deleteTodo}: TodoItemProps) {
+  const [isDeleting, setIsDeleting] = useState(false);
+
+const handleDelete = async () => {
+    setIsDeleting(true);
+    await deleteTodo(id);
+    setIsDeleting(false);
+  };
+
   return (
     <li className="flex gap-1 items-center">
       <input
@@ -17,6 +27,13 @@ export function TodoItem({id, title, complete, toggleTodo}: TodoItemProps) {
       />
 
       <label htmlFor={id} className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500">{title}</label>
+      <button
+        onClick={handleDelete}
+        disabled={isDeleting}
+        className="border border-red-500 text-red-500 px-2 py-1 rounded hover:bg-red-700 hover:text-white focus-within:bg-red-700 focus-within:text-white outline-none ml-auto"
+      >
+        {isDeleting ? 'Deleting...' : 'Delete'}
+      </button>
     </li>
 )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { prisma } from '@/app/db';
 import { TodoItem } from "@/app/components/TodoItem";
+import { revalidatePath } from 'next/cache';
 
 function getTodos() {
   return prisma.todo.findMany();
@@ -11,17 +12,23 @@ async function toggleTodo(id: string, complete: boolean) {
   await prisma.todo.update({where: {id}, data: {complete}});
 }
 
+async function deleteTodo(id: string) {
+  "use server"
+  await prisma.todo.delete({where: {id}});
+  revalidatePath('/');
+}
+
 export default async function Home() {
   const todos = await getTodos();
   return (
   <>
-  <header className="flex justify-between items-center">
+  <header className="flex justify-between items-center mb-4">
     <h1 className="text-2xl">Todos</h1>
     <Link className="border border-slate-300 text-slate-300 px-2 py-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
   </header>
   <ul className="pl-4">
     {todos.map(todo => (
-      <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
+      <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     ))}
   </ul>
   </>
